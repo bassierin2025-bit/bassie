0562b5f8607b4e8707fa5937b105c1be2460c885
bassierin2025-bit <bassierin2025@gmail.com>
2026-06-12 14:40:52 +1000

    Refine app layout and add custom color picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'listening-diary-v8';
+const CACHE_NAME = 'listening-diary-v9';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './supabase-config.js'];
 
 self.addEventListener('install', event => {
