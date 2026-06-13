d50f3feb78c4507029a45bd611f3f74630eda520
bassierin2025-bit <bassierin2025@gmail.com>
2026-06-13 13:36:38 +1000

    Sync charts and heatmaps with custom theme colors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'listening-diary-v10';
+const CACHE_NAME = 'listening-diary-v11';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './supabase-config.js'];
 
 self.addEventListener('install', event => {
